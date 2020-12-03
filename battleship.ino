b9ed23bdde190bdc881926571d98bcac43280327
Teablack <34287403+Teablack@users.noreply.github.com>
2020-12-03 18:30:18 +0100

    +statki 2os
    00 22 44 66 99 98 dziala
    00 22 44 66 99 89 nie dziala problem w is_neighbor?
    2 statki wczytuje tylko 1.5 razy w czym problem?

--- battleship.ino
+++ battleship.ino
@@ -29,7 +29,21 @@ void show_field(){//wyświetlam pole gry
   	}
 }
 
-bool check_pos(int row,int column){
+bool is_neighbor(int row,int column,int old_row,int old_column){
+	if(row==old_row &&((column-1==old_column && column!=0) || (column+1==old_column && column!=9) )) return true;
+  	if(column==old_column &&((row-1==old_row && row!=0)|| (row+1==old_row)&&row!=9 )) return true;
+	return false;
+}
+
+bool check_pos_for_2(int row,int column){
+  if(row && check_pos(row-1,column,11,11)) return true;
+  if(row!=9 && check_pos(row+1,column,11,11)) return true;
+  if(column && check_pos(row,column-1,11,11)) return true;
+  if(column!=9 && check_pos(row,column+1,11,11)) return true;
+return false;
+}
+
+bool check_pos(int row,int column,int old_row,int old_column){
   int 	left=	column-1,		
   		right=	column+1,
   		top=	row-1,
@@ -42,7 +56,7 @@ bool check_pos(int row,int column){
     
   for(int i=left;i<=right;i++){
   	for(int j=top;j<=bottom;j++){
-      if(field[i][j]) return false;
+      if(field[i][j] && i!=old_row && j!=old_column) return false;
   	}
   }
 	return true;	
@@ -56,24 +70,66 @@ void read_1ship_pos(){	//czytam pojedynczy statek
       	Serial.print("wpisz kolumne\n");
       	column=read_col_or_row();
     
-    is_goodpos=check_pos(row,column); 
+    is_goodpos=check_pos(row,column,11,11); 
   	if(is_goodpos){
     	field[row][column]=1;
     }
-  	else Serial.print("błędny krok!\n");
+  	else Serial.print("błędny krok!Wpisz jeszcze raz\n");
     }
+}
+
+void read_2ship_pos(){
+	bool is_goodpos=false;
+  	int row,column;
+  	while(!is_goodpos){
+      	Serial.print("wpisz wiersz\n");
+  		row=read_col_or_row();
+      	Serial.print("wpisz kolumne\n");
+      	column=read_col_or_row();
+    
+    	is_goodpos=check_pos(row,column,11,11);
+    	//teraz sprawdz czy jest miejsce dla 2 klocka
+    	if(is_goodpos){
+      		is_goodpos=check_pos_for_2(row,column);
+       		if(is_goodpos){
+    			field[row][column]=1;
+    		}
+    	}
+  		else Serial.print("błędny krok!Wpisz jeszcze raz\n");
+    }
+  		int old_row=row,old_column=column;
+  //wstawic drugi po sasiedstwu i sprawdzic jak 1
+    	while(!is_goodpos){
+      		Serial.print("wpisz wiersz\n");
+  			row=read_col_or_row();
+      		Serial.print("wpisz kolumne\n");
+      		column=read_col_or_row();
+          	//sprawdz czy sasiaduja
+          	is_goodpos= is_neighbor(row,column,old_row,old_column);
+    		is_goodpos= is_goodpos &&(check_pos(row,column,old_row,old_column));
+    		if(is_goodpos){
+    			field[row][column]=1;
+    		}
+  			else Serial.print("błędny krok!Wpisz jeszcze raz\n");
+    	}
 }
 
 void read_ships(int count){
 	switch (count){
       case 4:
       	Serial.print("Wpisz pozycje statkow1\n");
-      	for(int i=0;i<count;i++)read_1ship_pos();
+      for(int i=0;i<count;i++){
+        read_1ship_pos();
       	show_field();
-        break;	/*	
+      }
+        break;		
       case 3:
-        for(int i=0;i<count;i++)read_2ship_pos();
-        break;
+      Serial.print("Wpisz pozycje statkow2\n");
+      for(int i=0;i<count;i++){
+        read_2ship_pos();
+      	show_field();
+      }
+      break;/*
       case 2:
       	read_3ship_pos();
       	read_3ship_pos();
